22d9a31af30afdcece752c03d204a4174f1a13fc
janghs <gorae930301@gmail.com>
2024-05-14 10:31:10 +0900

    인증 방식 커스텀

---
+++ src/main/java/com/example/securitytesttoken/service/CustomUserDetailsService.java
@@ -0,0 +1,9 @@
+package com.example.securitytesttoken.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface CustomUserDetailsService extends UserDetailsService {
+
+	// 새로운 인증 방식 사용
+
+}
---
+++ src/main/java/com/example/securitytesttoken/serviceImpl/CustomUserDetailsServiceImpl.java
@@ -0,0 +1,16 @@
+package com.example.securitytesttoken.serviceImpl;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.example.securitytesttoken.service.CustomUserDetailsService;
+
+public class CustomUserDetailsServiceImpl implements CustomUserDetailsService {
+
+	@Override
+	// 사용하지 않고 CustomUserDetailsService에서 새로운 인증 방식을 만들어서 사용
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return null;
+	}
+
+}
