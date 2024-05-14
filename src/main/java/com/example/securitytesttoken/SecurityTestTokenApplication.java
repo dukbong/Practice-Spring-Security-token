2963784da79bc10e426072ff02a4d67576b7b183
janghs <gorae930301@gmail.com>
2024-05-14 09:31:34 +0900

    firstCommit🎉

---
+++ src/main/java/com/example/securitytesttoken/SecurityTestTokenApplication.java
@@ -0,0 +1,13 @@
+package com.example.securitytesttoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityTestTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityTestTokenApplication.class, args);
+	}
+
+}
